3bbd68b3611bab3792f8d515bf1ddc9da3ba5b9a
Kyle Lin <kylejlin.us@gmail.com>
2019-06-05 19:33:18 -0700

    Add concrete adaptors for BatchChoices

--- src/choices/batch_choice.rs
+++ src/choices/batch_choice.rs
@@ -36,3 +36,90 @@ pub enum BatchChoices {
     Actions(Vec<Vec<Action>>),
     None,
 }
+
+impl BatchChoices {
+    pub fn characters(self) -> Option<Vec<Vec<Character>>> {
+        if let BatchChoices::Characters(characters) = self {
+            Some(characters)
+        } else {
+            None
+        }
+    }
+
+    pub fn boosters(self) -> Option<Vec<Vec<Booster>>> {
+        if let BatchChoices::Boosters(boosters) = self {
+            Some(boosters)
+        } else {
+            None
+        }
+    }
+
+    pub fn dequeue_choices(self) -> Option<Vec<Vec<DequeueChoice>>> {
+        if let BatchChoices::DequeueChoices(dequeue_choices) = self {
+            Some(dequeue_choices)
+        } else {
+            None
+        }
+    }
+
+    pub fn actions(self) -> Option<Vec<Vec<Action>>> {
+        if let BatchChoices::Actions(actions) = self {
+            Some(actions)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn characters_returns_some_if_variant_is_characters() {
+        let choices = BatchChoices::Characters(vec![]);
+        assert!(choices.characters().is_some());
+    }
+
+    #[test]
+    fn characters_returns_none_if_variant_is_not_characters() {
+        let choices = BatchChoices::Boosters(vec![]);
+        assert!(choices.characters().is_none());
+    }
+
+    #[test]
+    fn boosters_returns_some_if_variant_is_boosters() {
+        let choices = BatchChoices::Boosters(vec![]);
+        assert!(choices.boosters().is_some());
+    }
+
+    #[test]
+    fn boosters_returns_none_if_variant_is_not_boosters() {
+        let choices = BatchChoices::Characters(vec![]);
+        assert!(choices.boosters().is_none());
+    }
+
+    #[test]
+    fn dequeue_choices_returns_some_if_variant_is_dequeue_choices() {
+        let choices = BatchChoices::DequeueChoices(vec![]);
+        assert!(choices.dequeue_choices().is_some());
+    }
+
+    #[test]
+    fn dequeue_choices_returns_none_if_variant_is_not_dequeue_choices() {
+        let choices = BatchChoices::Actions(vec![]);
+        assert!(choices.dequeue_choices().is_none());
+    }
+
+    #[test]
+    fn actions_returns_some_if_variant_is_actions() {
+        let choices = BatchChoices::Actions(vec![]);
+        assert!(choices.actions().is_some());
+    }
+
+    #[test]
+    fn actions_returns_none_if_variant_is_not_actions() {
+        let choices = BatchChoices::None;
+        assert!(choices.actions().is_none());
+    }
+}
